eed63f6df07b7b7203e3a195ef637dc640e5e6dc
Tinajero <ruiztinajero@gmail.com>
2014-10-28 08:56:43 -0600

    listo

--- laplaciano.m
+++ laplaciano.m
@@ -1,17 +1,15 @@
 
 imtool close all
-im = imread('C:\Users\Alumnos\Documents\MATLAB/images/tools3.bmp');
+im = imread('C:\Users\Alumnos\Documents\MATLAB/images/reloj.bmp');
 [M, N ] = size(im);
 w1 = [ -1 -1 -1 ; -1 8 -1; -1 -1 -1 ];
 w2 = [ 1 1 1; 1 -8 1; 1 1 1 ];
 w3 = [ 0 -1 0 ; -1 4 -1 ; 0 -1 0 ];
 w4 = [ 0 1 0 ; 1 -4 1 ; 0 1 0 ];
 
-
-lap = imfilter(im, w4);
+lap = imfilter(im, w1);
 
 g = zeros(M, N);
-
 
 for i = 1:M
     for j = 1:N
