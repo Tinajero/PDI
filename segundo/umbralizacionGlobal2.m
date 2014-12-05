427975d8b8889c23d1d9b19ff3785868655a4288
Tinajero <ruiztinajero@gmail.com>
2014-12-05 08:50:20 -0600

    algo

--- segundo/umbralizacionGlobal2.m
+++ segundo/umbralizacionGlobal2.m
@@ -8,10 +8,10 @@ g = zeros(M,N);
 deltaT = 2;
 
 
-g1 = ima > T;
-g2 = ima <= T;
+    g1 = ima > T;
+    g2 = ima <= T;
 
 
-indicesg1 = find( ima> T);
-indicesg2 = find( ima <= T);
+    indicesg1 = find( ima> T);
+    indicesg2 = find( ima <= T);
 
