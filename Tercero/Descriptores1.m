e4072f26808afb6db77a4ae36702f01de6c25d68
tinajero <ruiztinajer@gmail.com>
2015-01-08 08:56:41 -0600

    descriptores

--- Tercero/Descriptores1.m
+++ Tercero/Descriptores1.m
@@ -1,14 +1,7 @@
-
-% usando la umbralizacion se obtiene dos regionezs la region 1 que es el
-% fondo y la 2 que es el objeto
-% se tiene que encontrar sus borde ya sea con 4 conectividad o 8
-% conectividad
-% despues aplicar la representacion del borde con el codigo de cadena
-
-% practica codigos de cadena 7/01/2014 hecha
-ima = imread('C:\Users\Alumnos\Documents\MATLAB\PDI2\im\bola23.bmp');
+ima = imread('C:\Users\Alumnos\Documents\MATLAB\PDI2\im\circuloDeformado.bmp');
 imtool close all
 [M,N] = size(ima);
+
 T = 78;
 Tn = 360;
 g = zeros(M,N);
@@ -28,13 +21,9 @@ while( abs(T-Tn) > deltaT )
     T = (m1 +m2 ) / 2;
 end
 
-%T
 Ans = ima > T;
 imtool(Ans,[]);
-Ans2 = Ans;
-Ans3 = Ans;
-
-% haciendo 4 conectividad
+Ans2 = Ans
 for i = 1:M
     for j = 1:N
          bandera = 1;
@@ -56,32 +45,9 @@ for i = 1:M
     end
 end
                         
-imtool(Ans2,[]);     
+imtool(Ans2,[]);
 movx = [0,1,0,-1];
 movy = [-1,0,1,0];
-% haciendo 8 conectividad
-for i = 1:M
-    for j = 1:N
-         bandera = 1;
-        for ind = 1:4
-           
-            x =  movx(ind);
-            y = movy(ind);
-                if i + x < M && j + y < N && i + x > 0 && j + y > 0
-                    if ( Ans(i+x,j+y) == 1 )
-                        %Ans2(i,j) = 1
-                        bandera = 0;
-                    end 
-                end 
-        end
-        if bandera == 1
-            Ans3(i,j) = 1;
-        end
-    end
-end
-
-imtool(Ans3,[]);
-
 temp = Ans2;    
 for i = 1:M
     for j = 1:N
@@ -92,10 +58,8 @@ for i = 1:M
     end
 end
 
-cadena = zeros(1000,1);
 cola1 = 1;
 for i = 1 : 1000
-  
     for ind = 1:4
         x = ix + movx(ind);
         y = iy + movy(ind);
@@ -103,42 +67,71 @@ for i = 1 : 1000
             temp(ix,iy) = 1;
             ix = x;
             iy = y;
-            
-            cadena(cola1) = ind-1;
             cola1=cola1+1;
             break;
         end
     end
 end
 
-%imtool(temp,[]);
-temp = Ans3;
-movx = [0,1,1,1,0,-1,-1,-1];
-movy = [-1,-1,0,1,1,1,0,-1];
-cadena2 = zeros(1000,1);
-cola = 1;
+perimetro1 = cola1
+
+
+for i = M:1
+    for j = N:1
+        if(Ans2(i,j) == 0)
+            ix = i;
+            iy = j;
+        end
+    end
+end
+
+temp = Ans;
+cola1 = 1;
 for i = 1 : 1000
-  
-    for ind = 1:8
+    for ind = 1:4
         x = ix + movx(ind);
         y = iy + movy(ind);
         if temp(x,y) == 0
             temp(ix,iy) = 1;
             ix = x;
             iy = y;
-            
-            cadena2(cola) = ind-1;
-            cola=cola+1;
+            cola1=cola1+1;
             break;
         end
     end
 end
-imtool(temp,[]);
+
+perimetro2 = cola1
 
 
+colaSimul = zeros(1000,2);
 
+tope = 2;
+actx = 147 ;
+acty = 100 ;
 
+colaSimul(1,1) = actx;
+colaSimla(1,2) = acty;
+temp = Ans2;
+cont = 0;
+while tope ~= 1
+    actx = colaSimul(tope-1, 1);
+    acty = colaSimul(tope-1, 2);
+    for ind = 1:4
+        x = actx + movx(ind);
+        y = acty + movy(ind);
+        if x > 0 && y > 0 
+        if temp(x,y) == 1
+            temp(x,y) = 0;
+            cont=cont+1;
+            colaSimul(tope, 1) = x;
+            colaSimul(tope, 2) = y;
+            tope = tope+1;
+           
+        end
+        end
+    end
+end
 
-
-
+area1 = cont
 
