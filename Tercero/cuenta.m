ab0f3e86a7f9639933ac3f520a3d79e7c7bb1c81
Tinajero <ruiztinajero@gmail.com>
2015-01-20 01:20:56 -0600

    tarea , reaizada segun

--- Tercero/cuenta.m
+++ Tercero/cuenta.m
@@ -1,23 +1,29 @@
 
-function [cont0,cont45,cont90] = cuenta (a,b,d,ima,M,N)
-cont = 0;
+function [cont0,cont45,cont90,cont135] = cuenta (a,b,d,ima,M,N)
+
+cont90 = 0;
+cont0 = 0;
 cont45 = 0;
+cont135 = 0;
     for i = 1: M
         for j = 1: N
 
             if ( j+d <= N && (ima(i,j) == a && ( ima(i,j+d) ==b) || (j-d>0 && ima(i,j) == a && ima(i,j-d) ==b) ) )
-                cont= cont+1;
+                cont0= cont0+1;
             end
             if( j+d <=N && i +d <=M && j-d >0 && i-d > 0 && ( (ima(i,j) == a && ima(i+d,j+d) ==b)|| (ima(i,j) == a && ima(i-d,j-d) == b) ) )
                 
                 cont45=cont45+1;
             end
             
-            if(j+d <=N &&  j-d >0 && ( (ima(i,j)==a && ima(i+d,j)==b) || (ima(i,j)==a && ima(i+d,j)) ) )
+            if(i+d <=M &&  j-d >0 && ( (ima(i,j)==a && ima(i+d,j)==b) || (ima(i,j)==a && ima(i+d,j)) ) )
                 cont90=cont90+1;
+            end
+             if( j+d <=N && i +d <=M && j-d >0 && i-d > 0 && ( (ima(i,j) == a && ima(i+d,j-d) ==b)|| (ima(i,j) == a && ima(i-d,j+d) == b) ) )
+                
+                cont135=cont135+1;
             end
             
         end
     end
-    cont0 = cont;
 end
--- Tercero/descriptores2orden.m
+++ Tercero/descriptores2orden.m
@@ -1,7 +1,8 @@
 %Practica descriptores de segundo orden
 %late
 %19/01/2015
-ima = imread('C:\Users\Alumnos\Documents\MATLAB\PDI\Tercero\floor1-a-p002.png');
+%ima = imread('C:\Users\Alumnos\Documents\MATLAB\PDI\Tercero\floor1-a-p002.png');
+ima = imread('E:\Documentos+\2015 A\PDI\PDI\imagenes\floor1-a-p002.png');
 % canvas1-a-p001
 % ceiling1-a-p001
 % floor1-a-p002
@@ -12,16 +13,25 @@ imtool close all
 
 emed0 = zeros(255,255);
 emed45 = zeros(255,255);
-emede135 = zeros(255,255);
-emede90 = zeros(255,255);
-
-for i = 1:3
-    for j= 1:3
+emed135 = zeros(255,255);
+emed90 = zeros(255,255);
+s0 = 0;
+s45 = 0;
+s90 = 0;
+s135 = 0;
+L = max(max(ima))
+for i = 1:L
+    for j= 1:L
         a= i;
         b=j;
         d = 1;
-        emed0(i,j) = cuenta(a,b,d,ima,M,N);%(a,b,d,ima,M,N)
-        
+        [emed0(i,j),emed45(i,j), emed90(i,j),emed135(i,j)]= cuenta(a,b,d,ima,M,N);%(a,b,d,ima,M,N)
+        s0 = s0 + emed0(i,j);
+        s45 = s45 + emed45(i,j);
+        s90 = s90 + emed90(i,j);
+        s135 = s135 + emed135(i,j);
     end
 end
+
+11
 
